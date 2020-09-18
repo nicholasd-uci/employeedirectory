266387e27901142355a94e3de747642a868aa6a1
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-09-17 19:26:15 -0700

    rendered app.js with employee info and axios request

--- src/App.js
+++ src/App.js
@@ -5,11 +5,51 @@ import ReactTable from 'react-table-v6'
 
 const App = () => {
 
+const [employeeState, setEmployeeState] = useState({
+    employees: [],
+    columns: [
+        {
+            Header: 'Name',
+            accessor: 'name',
+            filterable: true
+        },
+        {
+            Header: 'Phone',
+            accessor: 'phone',
+            filterable: true
+        },
+        {
+            Header: 'Email',
+            accessor: 'email'
+        },
+        {
+            Header: 'Gender',
+            accessor: 'gender'
+        }
+    ]
+})
+
+userEffect(() => {
+    axios.get('https://randomuser.me/api?results=20')
+    .then(({ data: { results } }) => {
+        let employees = results.map(user => ({
+            name: employee.name.first + '' + employee.name.last,
+            phone: employee.phone,
+            email: employee.email,
+            gender: employee.gender
+        }))
+        // code goes here
+        setEmployeeState({ ...employeeState, employees})
+    })
+    .catch(err => console.log(err))
+}, [])
 
     return(
-        <>
-        <h1>Hello World!</h1>
-        </>
+        <ReactTable
+        data={employeeState.employees}
+        columns={employeeState.columns}
+        />
     )
 }
+
 export default App
