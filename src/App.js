37e81ff183c7d61d186d19f5227a0a2ac1d5eecc
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-09-17 18:56:30 -0700

    tested working server with Hello World Test

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
+import React, { useState, useEffect } from 'react'
+import 'react-table-v6/react-table.css'
+import axios from 'axios'
+import ReactTable from 'react-table-v6'
 
+const App = () => {
+
+
+    return(
+        <>
+        <h1>Hello World!</h1>
+        </>
+    )
+}
+export default App
